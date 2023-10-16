cb09c0d0a2e77c103f3cc4ecf63cde06616de4fc
art0007i <art0007i@gmail.com>
2023-10-16 21:35:30 +0200

    Add project files.

---
+++ DefaultToolOverride/DefaultToolOverride.cs
@@ -0,0 +1,214 @@
+using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+using System.Reflection.Emit;
+using Elements.Core;
+
+namespace DefaultToolOverride
+{
+    public class DefaultToolOverride : ResoniteMod
+    {
+        public override string Name => "DefaultToolOverride";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/DefaultToolOverride/";
+
+        public enum OverrideType
+        {
+            Fallback, // Use default behavior, so spawn the default resonite tools
+            None, // Don't do anything at all, no tool will be equipped or dequipped
+            Dequip, // Dequip and stash the current tool. same as pressing 1 by default
+            URL, // Spawn a custom tool from a url
+            ClassName // Spawn a tool by attaching the provided tool component to an empty slot
+        }
+
+        public struct ToolReplacement
+        {
+            public OverrideType Type => config.GetValue(type);
+            public string Str => config.GetValue(str);
+
+            Type _lastType = null;
+
+            public Type ToolType
+            {
+                get
+                {
+                    var st = Str;
+                    if (_lastType == null || !st.Contains(_lastType.Name))
+                    {
+                        _lastType = AccessTools.TypeByName(st);
+                    }
+                    return _lastType;
+                }
+            }
+
+            ModConfigurationKey<OverrideType> type;
+            ModConfigurationKey<string> str;
+
+            public ToolReplacement(ModConfigurationKey<OverrideType> type, ModConfigurationKey<string> str)
+            {
+                this.type = type;
+                this.str = str;
+            }
+
+        }
+
+        public static Dictionary<int, ToolReplacement> ConfigKeys = new();
+
+        public static ModConfiguration config;
+
+        private void BuildToolKey(ModConfigurationDefinitionBuilder builder, string name, int index)
+        {
+            var key_type = new ModConfigurationKey<OverrideType>($"tool{name}_type", $"Tool {name} will use this mode.", () => OverrideType.Fallback);
+            var key_string = new ModConfigurationKey<string>($"tool{name}_string", $"String for Tool {name}. (Only for URL / ClassName)", () => null);
+            builder.Key(key_type);
+            builder.Key(key_string);
+            ConfigKeys.Add(index, new(key_type, key_string));
+        }
+
+
+        public override void DefineConfiguration(ModConfigurationDefinitionBuilder builder)
+        {
+            base.DefineConfiguration(builder);
+            for (int i = 1; i < 10; i++)
+            {
+                BuildToolKey(builder, i.ToString(), i);
+            }
+            BuildToolKey(builder, "0", 0);
+            BuildToolKey(builder, "minus", 10);
+        }
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+
+            Harmony harmony = new Harmony("me.art0007i.DefaultToolOverride");
+            harmony.PatchAll();
+
+        }
+        [HarmonyPatch(typeof(InteractionHandler), "OnInputUpdate")]
+        class DefaultToolOverridePatch
+        {
+            public static bool ToolIntercept(int toolNum, InteractionHandler instance)
+            {
+                if (ConfigKeys.TryGetValue(toolNum, out var pair))
+                {
+                    switch (pair.Type)
+                    {
+                        case OverrideType.Fallback:
+                            return true;
+                        case OverrideType.Dequip:
+                            instance.StashCurrentToolOrDequip();
+                            break;
+                        case OverrideType.URL:
+                            if (Uri.TryCreate(pair.Str, UriKind.Absolute, out var uri))
+                            {
+                                instance.StartTask(async delegate
+                                {
+                                    await instance.SpawnAndEquip(uri);
+                                });
+                            }
+                            else
+                            {
+                                Warn($"Invalid tool url '{pair.Str}' on tool {toolNum}.");
+                            }
+                            break;
+                        case OverrideType.ClassName:
+                            var toolType = pair.ToolType;
+                            if (toolType != null && typeof(ITool).IsAssignableFrom(toolType))
+                            {
+                                // froox uses Uri as dictionary keys. why? I don't know, but you can shove any random url in and it will take it without issue.
+                                SpawnEquipFunc(instance, new Uri("tool://tool" + toolNum), toolType);
+                            }
+                            else
+                            {
+                                Warn($"Invalid tool TypeName '{pair.Str}' on tool {toolNum}.");
+                            }
+                            break;
+                    }
+                    return false;
+                }
+                return true;
+            }
+
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+            {
+                Label lastLabel = default;
+                foreach (var code in codes)
+                {
+                    if (code.Branches(out var label))
+                    {
+                        if (label.HasValue) lastLabel = label.Value;
+                    }
+                    if (code.opcode == OpCodes.Switch && code.operand is Label[] arr && arr.Length > 6)
+                    {
+                        yield return new(OpCodes.Ldarg_0); // load object instance so we can access it in our intercept function
+                        yield return new(OpCodes.Call, typeof(DefaultToolOverridePatch).GetMethod(nameof(DefaultToolOverridePatch.ToolIntercept)));
+                        yield return new(OpCodes.Brfalse, lastLabel); // if our intercept says false skip remaining code
+                        yield return new(OpCodes.Ldloc_S, (byte)12); // put the int back so the real switch statement can use it
+                    }
+                    yield return code;
+                }
+            }
+
+            // Copy of InteractionHandler.SpawnAndEquip with minor tweaks
+            public static void SpawnEquipFunc(InteractionHandler instance, Uri uri, Type toolType)
+            {
+                var t = Traverse.Create(instance);
+                var uriField = t.Field<Uri>("_currentToolUri");
+                if (uriField.Value == uri)
+                {
+                    return;
+                }
+                Slot toolSlot = null;
+                bool cleanup = false;
+                try
+                {
+                    var _stashedTools = t.Field<Dictionary<Uri, ITool>>("_stashedTools").Value;
+                    if (_stashedTools.TryGetValue(uri, out var value) && !value.IsRemoved)
+                    {
+                        value.Slot.ActiveSelf = true;
+                        toolSlot = value.Slot;
+                    }
+                    else
+                    {
+                        // only piece of my code in this entire func lmao
+                        toolSlot = instance.LocalUserRoot.Slot.AddSlot(toolType.Name);
+                        toolSlot.AttachComponent(toolType);
+                        value = toolSlot.GetComponentInChildren<ITool>();
+                    }
+                    _stashedTools.Remove(uri);
+                    if (value == null)
+                    {
+                        cleanup = true;
+                    }
+                    else if (!instance.CanEquip(value))
+                    {
+                        cleanup = true;
+                        NotificationMessage.SpawnTextMessage(value.Slot, "You don't have permission to equip this.", colorX.Red, 3f, 0.15f, 0.5f, 0.1f);
+                    }
+                    else
+                    {
+                        instance.StashCurrentToolOrDequip();
+                        instance.Equip(value, lockEquip: true);
+                        uriField.Value = uri;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error("Exception trying to spawn and equip tooltip:\n" + ex);
+                    cleanup = true;
+                }
+                if (cleanup)
+                {
+                    toolSlot?.Destroy();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# DefaultToolOverride
+
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that gives you full control over the desktop number row tool keybinds.
+
+This mod allows rebinding the 0-9 keys (and minus) in five different ways:
+- Fallback: The key will behave exactly the same as it would without the mod.
+- None: The key will do nothing. Can be useful if you want to create a multitool for desktop mode.
+- Dequip: Dequips and stashes the currently held tool. aka the default action of the `1` key.
+- URL: Spawns a tool from the record / asset URL specified inside the "string" config key.
+- ClassName: Spawns an empty slot with the tool component specified in the "string" config key. This is useful because a lot of the tools will generate a basic visual when attached. Examples can be found in the `DefaultClassNames.json` file which contains all the default tools, but as ClassNames.
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [DefaultToolOverride.dll](https://github.com/art0007i/DefaultToolOverride/releases/latest/download/DefaultToolOverride.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
